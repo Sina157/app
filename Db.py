b1d625bc7a505c6064ec13b367a1556f46496329
Mohammad <95890589+Sina157@users.noreply.github.com>
2024-01-29 10:48:15 +0330

    Add files via upload

---
+++ Db.py
@@ -0,0 +1,58 @@
+import sqlite3
+
+DbName = "DbApp.sqlite3"
+
+def CreateTables(DataBaseName=DbName):
+        conn = sqlite3.connect(DataBaseName)
+        query = conn.cursor()
+        query.execute('''
+        CREATE TABLE IF NOT EXISTS Users
+        ([ID]  INTEGER PRIMARY KEY,
+        [visited]	INTEGER,
+        [submited]	INTEGER,
+        [IP]    TEXT,
+        [Scode]    TEXT
+        )
+        ''')
+    
+    
+def AddOrUpdate(IP , Scode , DataBaseName=DbName):
+            conn = sqlite3.connect(DataBaseName)
+            query = conn.cursor()
+            FoundUserID = query.execute(f'''
+                SELECT ID FROM Users WHERE IP = "{IP}" OR Scode = "{Scode}";
+                ''').fetchone()
+            if (FoundUserID != None):
+                query.execute(f'''
+                    UPDATE Users
+                    SET submited = submited + 1
+                    WHERE ID = "{FoundUserID[0]}";
+                ''')
+            else:
+                query.execute(f'''
+                        INSERT INTO Users (IP , Scode , visited , submited)
+                                VALUES
+                                ("{IP}", "{Scode}" ,0, 1)
+                        ''')
+            conn.commit()
+
+def onVisitEvent(IP, DataBaseName=DbName):
+    conn = sqlite3.connect(DataBaseName)
+    query = conn.cursor()
+    query.execute(f'''
+                    UPDATE Users
+                    SET visited = visited + 1
+                    WHERE IP = "{IP}";
+                ''')
+    conn.commit()
+
+def GetUserByIP(IP, DataBaseName=DbName):
+    conn = sqlite3.connect(DataBaseName)
+    query = conn.cursor()
+    User = query.execute(f'''
+                SELECT * FROM Users WHERE IP = "{IP}";
+                ''').fetchone()
+    return User
+
+CreateTables()
+onVisitEvent("")
---
+++ app.py
@@ -0,0 +1,77 @@
+from flask import Flask, render_template, request , jsonify
+import Db
+import requests
+import jdatetime
+from pytz import timezone
+
+DaysOfWeek = {
+    "Saturday":"شنبه","Sunday":"یکشنبه","Monday":"دوشنبه","Tuesday":"سه‌شنبه","Wednesday":"چهارشنبه","Thursday":"پنجشبه","Friday":"جمعه"
+}
+Months = {
+    "1":"فروردین","2":"اردیبهشت","3":"خرداد","4":"تیر","5":"مرداد","6":"شهریور","7":"مهر","8":"آبان","9":"آذر","10":"دی","11":"بهمن","12":"اسفند"
+}
+
+def SendMessageToTelegramDirect(Message):
+    requests.get("https://api.telegram.org/bot6744041909:AAE-DQD8TuJpVAWZv0UDKL3_8YAcJetblmU/sendmessage?chat_id=151372864&text="+Message)
+
+def SendMessageToTelegramIndirect(Message):
+    settings = {
+        "UrlBox": "https://api.telegram.org/bot6744041909:AAE-DQD8TuJpVAWZv0UDKL3_8YAcJetblmU/sendmessage?chat_id=151372864&text="+Message,
+        "MethodList": "POST"
+    }
+    req = requests.post(
+        "https://www.httpdebugger.com/tools/ViewHttpHeaders.aspx", settings)
+    return req
+
+
+app = Flask(__name__ , template_folder=".")
+
+
+def SendToTelegram(f1 , f2 , f3 , visited , submited , id):
+    message = f"""
+    کاربر شماره: {id}
+    کد اشتراکی دریافتی از ادمین:
+    {f1}
+    کد انتخاب شده:
+    {f2}
+    تاریخ و زمان رسید:
+    {f3}
+    """
+    if submited > 1:
+        now = jdatetime.datetime.now(timezone('Asia/Tehran'))
+        day = DaysOfWeek.get(now.strftime("%A")) 
+        time = now.strftime("%H:%M")  
+        Month = Months.get(now.strftime("%m"))
+        DayOfMonth = now.strftime("%d") 
+        message += f"""
+        کاربر تکراری هست هشدار🔴
+        {visited} بار وارد سایت شده 
+        {submited} بار فیلد هارو پر کرده 
+        {time} {day} {DayOfMonth} {Month} 
+        """
+    SendMessageToTelegramIndirect(message)
+
+@app.route('/', methods=['GET', 'POST'])
+def form_page():
+    Ip = request.environ.get('HTTP_X_REAL_IP', request.remote_addr) 
+    IsFromIran = True  # For Debug
+    # IsFromIran = requests.get(f"https://geolocation-db.com/json/{Ip}&position=true").json().get("country_name") == "Iran"
+    if not IsFromIran:
+        return "<h1>برای استفاده از سرویس فیلترشکن خود را خاموش کنید</h1>" 
+    try:
+        data = jsonify(request.json).get_json()
+        Scode = data.get('SecretCode')
+        if request.method == 'POST' and len(Scode) == 32:
+            Field1 = data.get('Field1')
+            Field2 = data.get('Field2')
+            Field3 = data.get('Field3')
+            Db.AddOrUpdate(Ip , Scode)
+            User = Db.GetUserByIP(Ip)
+            SendToTelegram(Field1,Field2,Field3,User[1],User[2],User[0])
+            return "Ok"
+    finally:
+        Db.onVisitEvent(Ip)
+        return render_template('form.html')
+
+if __name__ == '__main__':
+    app.run()
